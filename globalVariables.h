e25541001f4f015d01dad334041e2469a9f5b9cf
Marius <126407220+ASAPmarius@users.noreply.github.com>
2025-05-05 16:13:42 +0200

    Update globalVariables.h

--- globalVariables.h
+++ globalVariables.h
@@ -1,8 +1,8 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
-extern int serverPort = 4141;
-extern int INITIAL_CAPACITY = 2;
-extern int LOAD_FACTOR_THRESHOLD = 0.8;
+extern int serverPort;
+extern int INITIAL_CAPACITY;
+extern int LOAD_FACTOR_THRESHOLD;
 
 #endif
