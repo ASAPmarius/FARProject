13792103e38234ff03291fe5b5f3eaa97f1f27d9
aminekhiri <amine.khiri@etu.umontpellier.fr>
2025-05-07 10:10:26 +0200

    Update client.c

--- client.c
+++ client.c
@@ -21,10 +21,10 @@ typedef struct {
 // Thread pour envoyer les messages saisis au clavier
 void *sendThread(void *arg) {
     thread_arg_t *t = (thread_arg_t *)arg;
-    char msg[BUF_SIZE];
+    char msg[BUF_SIZE]; // Buffer pour le message(taille max 1000 caractères)
     while (fgets(msg, BUF_SIZE, stdin) != NULL) {
         size_t len = strlen(msg);
-        if (len > 0 && msg[len-1] == '\n') msg[len-1] = '\0';
+        if (len > 0 && msg[len-1] == '\n') msg[len-1] = '\0'; // Enlève le '\n' à la fin ajouté automatiquement par fgets
         if (sendto(t->sockfd, msg, strlen(msg), 0,
                    (struct sockaddr *)&t->servaddr, t->len) < 0) {
             perror("sendto");
@@ -52,6 +52,7 @@ void *recvThread(void *arg) {
 }
 
 int main(int argc, char *argv[]) {
+    //on lance le client avec l'adresse IP du serveur en argument
     if (argc < 2) {
         fprintf(stderr, "Usage: %s <server_ip>\n", argv[0]); // Adresse IP du serveur 
         exit(EXIT_FAILURE);
@@ -59,9 +60,9 @@ int main(int argc, char *argv[]) {
 
     // Demande du nom d'utilisateur
     printf("Entrez nom d'utilisateur: ");
-    char username[50];
+    char username[50]; // max 50 caractères pour le nom d'utilisateur
     if (!fgets(username, sizeof(username), stdin)) {
-        perror("fgets");
+        perror("fgets"); // Erreur de lecture du nom d'utilisateur
         exit(EXIT_FAILURE);
     }
     username[strcspn(username, "\n")] = '\0';
@@ -69,7 +70,7 @@ int main(int argc, char *argv[]) {
     // Création de la socket UDP
     int dS = socket(PF_INET, SOCK_DGRAM, 0);
     if (dS < 0) {
-        perror("socket");
+        perror("socket"); // Erreur de création de la socket
         exit(EXIT_FAILURE);
     }
 
@@ -78,15 +79,15 @@ int main(int argc, char *argv[]) {
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(serverPort);
-    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) { // Adresse IP du serveur dans argv[1] mais on peut changer
         perror("inet_pton");
         exit(EXIT_FAILURE);
     }
     socklen_t servlen = sizeof(servaddr);
 
     // Envoi de la commande de login
-    char login_msg[BUF_SIZE];
-    snprintf(login_msg, BUF_SIZE, "%s %s", LOGIN_CMD, username);
+    char login_msg[BUF_SIZE]; // Message de login
+    snprintf(login_msg, BUF_SIZE, "%s %s", LOGIN_CMD, username); // Format: "@login username"
     if (sendto(dS, login_msg, strlen(login_msg), 0,
                (struct sockaddr *)&servaddr, servlen) < 0) {
         perror("sendto login");
@@ -102,11 +103,11 @@ int main(int argc, char *argv[]) {
 
     // Lancement des threads d'envoi et de réception
     pthread_t tid_send, tid_recv;
-    if (pthread_create(&tid_send, NULL, sendThread, &arg) != 0) {
+    if (pthread_create(&tid_send, NULL, sendThread, &arg) != 0) { //création du thread d'envoi du message
         perror("pthread_create sendThread");
         exit(EXIT_FAILURE);
     }
-    if (pthread_create(&tid_recv, NULL, recvThread, &arg) != 0) {
+    if (pthread_create(&tid_recv, NULL, recvThread, &arg) != 0) { //création du thread de réception du message
         perror("pthread_create recvThread");
         exit(EXIT_FAILURE);
     }
